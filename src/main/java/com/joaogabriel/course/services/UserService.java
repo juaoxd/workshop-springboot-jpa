f251414806d897452a7e01048582191e9002aa77
João Gabriel <joaogabrielbochlivio@gmail.com>
2023-08-30 06:01:17 -0300

    feat: add exception handling on findById method

--- src/main/java/com/joaogabriel/course/services/UserService.java
+++ src/main/java/com/joaogabriel/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.joaogabriel.course.services;
 
 import com.joaogabriel.course.entities.User;
 import com.joaogabriel.course.repositories.UserRepository;
+import com.joaogabriel.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
---
+++ src/main/java/com/joaogabriel/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.joaogabriel.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+}
